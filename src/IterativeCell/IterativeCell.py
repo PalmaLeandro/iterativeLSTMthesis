5d166ae3119a85ce86a3868eefcbeff2c448ba89
Leandro Palma <lpalma@fi.uba.ar>
2016-12-30 17:10:51 -0300

    "update_inputs"

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -34,12 +34,12 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
 
     def __call__(self, input, state, scope=None):
         should_add_summary = tf.get_variable_scope().reuse is True and self._number_of_iterations_built is 0
-        self._iteration_activations = tf.ones(self.resolve_iteration_activations(input, state, input, state).get_shape())
+        self._iteration_activations = self.resolve_iteration_activations(input, state, input, state)
         output, new_state, number_of_iterations_performed = self.resolve_iteration_calculation(input, state, tf.zeros([]), scope)
         if should_add_summary:
             tf.histogram_summary("iterations_performed", number_of_iterations_performed,
                                  name="iterations_performed_summary")
-        return input + output, new_state
+        return output, new_state
 
     def resolve_iteration_calculation(self, input, state, number_of_iterations_performed, scope):
         old_c, old_h = array_ops.split(1, 2, state)
@@ -51,7 +51,7 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         # Only a new state is exposed if the iteration gate in this unit of this batch activated the extra iteration.
         new_h = new_h * self._iteration_activations + old_h * (1 - self._iteration_activations)
         new_c = new_c * self._iteration_activations + old_c * (1 - self._iteration_activations)
-        output = new_h
+        output = new_h + input
         new_state_to_next_iteration = array_ops.concat(1, [new_c, old_h])
         new_state_to_output = array_ops.concat(1, [new_c, new_h])
         if self._number_of_iterations_built < self._max_iterations:
