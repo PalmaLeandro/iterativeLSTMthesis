5b8142fb28c140fd06ff34d3eecf2fc07f768047
Leandro Palma <lpalma@fi.uba.ar>
2017-01-01 12:01:08 -0300

    "noresidual_activationfromlaststate_updateh_UPDATEINPUT_noprobdecay"

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -52,13 +52,13 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         # Only a new state is exposed if the iteration gate in this unit of this batch activated the extra iteration.
         new_h = new_h * self._iteration_activations + old_h * (1 - self._iteration_activations)
         new_c = new_c * self._iteration_activations + old_c * (1 - self._iteration_activations)
-        output = new_h
+        output = new_h + input
         new_state_to_next_iteration = array_ops.concat(1, [new_c, old_h])
-        new_state_to_output = array_ops.concat(1, [new_c, new_h])
+        new_state_to_output = array_ops.concat(1, [new_c, output])
         if self._number_of_iterations_built < self._max_iterations:
             self._iteration_activations = self.resolve_iteration_activations(input, state, output, new_state_to_output)
             return tf.cond(tf.equal(tf.reduce_max(self._iteration_activations), tf.constant(1.)),
-                           lambda: self.resolve_iteration_calculation(input + output,
+                           lambda: self.resolve_iteration_calculation(output,
                                                                       new_state_to_output,
                                                                       number_of_iterations_performed=
                                                                         number_of_iterations_performed,
@@ -67,7 +67,7 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         return output, new_state_to_output, number_of_iterations_performed
 
     def resolve_iteration_activations(self, input, old_state, output, new_state):
-        iteration_gate_logits = linear([input, new_state], self.output_size, True,
+        iteration_gate_logits = linear([new_state], self.output_size, True,
                                        scope=tf.get_variable_scope())
         iteration_activations = floor(sigmoid(iteration_gate_logits) + self._iterate_prob)
         #self._iterate_prob *= self._iterate_prob_decay
