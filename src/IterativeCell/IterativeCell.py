8da5b959d739cc3c46f01d4cdf80709abc211b02
Leandro Palma <lpalma@fi.uba.ar>
2016-12-31 21:40:19 -0300

    "noresidual_activationfromlastoutput_updateh_UPDATEINPUT_noprobdecay"

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -58,7 +58,7 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         if self._number_of_iterations_built < self._max_iterations:
             self._iteration_activations = self.resolve_iteration_activations(input, state, output, new_state_to_next_iteration)
             return tf.cond(tf.equal(tf.reduce_max(self._iteration_activations), tf.constant(1.)),
-                           lambda: self.resolve_iteration_calculation(input,
+                           lambda: self.resolve_iteration_calculation(input + output,
                                                                       new_state_to_next_iteration,
                                                                       number_of_iterations_performed=
                                                                         number_of_iterations_performed,
