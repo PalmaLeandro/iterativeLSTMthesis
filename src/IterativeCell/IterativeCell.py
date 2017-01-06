4882fafae399c362d95ced8ac0377c4ebd1e4a11
Leandro Palma <lpalma@fi.uba.ar>
2017-01-06 15:31:33 -0300

    "noresidual_activationfromnewstate_updateinputhcPARTIALLY_decayingprobactivation_nosinglecell"

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -48,13 +48,13 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         old_c, old_h = array_ops.split(1, 2, state)
         output, new_state = self._internal_nn(input, state, scope)
         tf.get_variable_scope().reuse_variables()
-        #new_c, new_h = array_ops.split(1, 2, new_state)
+        new_c, new_h = array_ops.split(1, 2, new_state)
         # Only a new state is exposed if the iteration gate in this unit of this batch activated the extra iteration.
-        new_state = new_state * self._iteration_activations + state * (1 - self._iteration_activations)
-        #new_c = new_c * self._iteration_activations + old_c * (1 - self._iteration_activations)
+        new_h = new_h * self._iteration_activations + old_h * (1 - self._iteration_activations)
+        new_c = new_c * self._iteration_activations + old_c * (1 - self._iteration_activations)
         output = output * self._iteration_activations + input * (1 - self._iteration_activations)
         #new_state_to_next_iteration = array_ops.concat(1, [new_c, old_h])
-        #new_state_to_output = array_ops.concat(1, [new_c, new_h])
+        new_state = array_ops.concat(1, [new_c, new_h])
         if self._number_of_iterations_built < self._max_iterations:
             iteration_activation_flag = floor(tf.reduce_max(self._iteration_activations) + self._iterate_prob)
             number_of_iterations_performed += iteration_activation_flag
