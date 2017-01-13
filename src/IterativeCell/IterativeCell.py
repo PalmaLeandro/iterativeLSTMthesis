8116c95ef22736b260ef451b0fe52001d2af7d21
Leandro Palma <lpalma@fi.uba.ar>
2017-01-12 22:26:11 -0300

    "IC_noresidual_activationfromoutput_updateinputstatePARTIALLY_decaying0.75probactivation0.5_nobatchnorcellreactivation_small"

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -76,14 +76,14 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
 
         number_of_iterations_performed += 1
         iteration_activations = self.resolve_iteration_activations(input, state, output, new_state, iterate_prob, current_iteration_activations)
-        output = tf.cond(self.loop_condition()(output, new_state, number_of_iterations_performed, iterate_prob, iteration_activations), lambda: input, lambda: output)
+        #output = tf.cond(self.loop_condition()(output, new_state, number_of_iterations_performed, iterate_prob, iteration_activations), lambda: input, lambda: output)
         iterate_prob = iterate_prob * tf.constant(self._iterate_prob_decay_constant)
         return output, new_state, number_of_iterations_performed, iterate_prob, iteration_activations
 
     def resolve_iteration_activations(self, input, old_state, output, new_state, iterate_prob, current_iteration_activations):
-        iteration_gate_logits = linear([input, output], self.output_size, True, scope=tf.get_variable_scope())
+        iteration_gate_logits = linear([output], self.output_size, True, scope=tf.get_variable_scope())
         tf.get_variable_scope().reuse_variables()
-        iteration_activations = tf.floor(sigmoid(iteration_gate_logits) + iterate_prob)
+        iteration_activations = sigmoid(iteration_gate_logits)
         return self.update_iteration_activations(current_iteration_activations, iteration_activations, iterate_prob)
 
     def update_iteration_activations(self, current_iteration_activations, new_iteration_activations, iterate_prob):
