a4b4f0ca746bfcb6ac9632493e375b657bc20d89
leandro <lpalma"fi.uba.ar>
2017-02-11 14:01:45 -0300

    Add inbound and outbound feature entropy calculation

--- src/IterativeCell/IterativeCell.py
+++ src/IterativeCell/IterativeCell.py
@@ -1,5 +1,3 @@
-from pexpect.expect import searcher_re
-
 import tensorflow as tf
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops.rnn_cell import linear
@@ -9,23 +7,25 @@ from tensorflow.python.ops import variable_scope as vs
 
 
 class IterativeCell(tf.nn.rnn_cell.RNNCell):
-    def __init__(self, internal_nn, max_iterations=10., iterate_prob=0.5, iterate_prob_decay=0.5, allow_cell_reactivation=True, add_summaries=False):
+    def __init__(self, internal_nn, iteration_activation_nn=None, max_iterations=10., initial_iterate_prob=0.75, iterate_prob_decay=0.75, allow_cell_reactivation=True, add_summaries=False):
         if internal_nn is None:
             raise "You must define an internal NN to iterate"
         if internal_nn.input_size!=internal_nn.output_size:
             raise "Input and output sizes of the internal NN must be the same in order to iterate over them"
+        if iteration_activation_nn is not None and (iteration_activation_nn.output_size!=internal_nn.output_size):
+            raise "Input and output sizes of the iteration activation NN should be the same as the ones from internal NN"
         if max_iterations<1:
             raise "The maximum amount of iterations to perform mus be a natural value"
-        if iterate_prob<=0 or iterate_prob>=1:
+        if initial_iterate_prob<=0 or initial_iterate_prob>=1:
             raise "iteration_prob must be a value between 0 and 1"
         self._internal_nn = internal_nn
+        self._iteration_activation_nn = iteration_activation_nn
         self._max_iteration_constant = max_iterations
-        self._initial_iterate_prob_constant = iterate_prob
+        self._initial_iterate_prob_constant = initial_iterate_prob
         self._iterate_prob_decay_constant = iterate_prob_decay
         self._allow_reactivation = allow_cell_reactivation
         self._should_add_summaries = add_summaries
         self._already_added_summaries = []
-
 
     @property
     def input_size(self):
@@ -40,26 +40,41 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         return self._internal_nn.state_size
 
     def __call__(self, input, state, scope=None):
-        loop_variables = [input,                                                    # previous layer output
-                          state,                                                    # last cell's state
-                          tf.zeros([input.get_shape()[0],input.get_shape()[1]]),    # number of the current iteration
-                          tf.constant(self._initial_iterate_prob_constant),         # initial iteration probability
-                          tf.ones([input.get_shape()[0],input.get_shape()[1]])]     # calculation of the first iteration's activation
-        final_output, \
-        final_state, \
-        number_of_iterations_performed, \
-        final_iterate_prob, \
-        final_iteration_activations = tf.while_loop(self.loop_condition(), self.loop_body(), loop_variables)
 
         if self._should_add_summaries:
-            self.add_summaries(final_output, final_state, number_of_iterations_performed, final_iterate_prob, final_iteration_activations)
+            self.add_pre_execution_summaries(input,state)
+
+        with vs.variable_scope(scope or type(self).__name__):
+            loop_variables = [input,                                                    # previous layer output
+                              state,                                                    # last cell's state
+                              tf.zeros([input.get_shape()[0],input.get_shape()[1]]),    # number of the current iteration
+                              tf.constant(self._initial_iterate_prob_constant),         # initial iteration probability
+                              tf.ones([input.get_shape()[0],input.get_shape()[1]])]     # calculation of the first iteration's activation
+            final_output, \
+            final_state, \
+            number_of_iterations_performed, \
+            final_iterate_prob, \
+            final_iteration_activations = tf.while_loop(self.loop_condition(), self.loop_body(), loop_variables)
+
+        if self._should_add_summaries:
+            self.add_post_execution_summaries(final_output, final_state, number_of_iterations_performed, final_iterate_prob, final_iteration_activations)
 
         return final_output, final_state
 
-    def add_summaries(self,final_output, final_state, number_of_iterations_performed, final_iterate_prob, final_iteration_activations):
+    def calculate_feature_entropy(self, feature_vector):
+        return feature_vector * tf.nn.log(feature_vector) + (1 - feature_vector) * tf.nn.log(1 - feature_vector)
+
+    def add_pre_execution_summaries(self, input, state):
+        if not self._already_added_summaries.__contains__(tf.get_variable_scope().name + "/pre_execution_input_entropy"):
+            variable_summaries(self.calculate_feature_entropy(input), tf.get_variable_scope().name + "/pre_execution_input_entropy")
+            self._already_added_summaries.append(tf.get_variable_scope().name + "/pre_execution_input_entropy")
+
+    def add_post_execution_summaries(self, final_output, final_state, number_of_iterations_performed, final_iterate_prob, final_iteration_activations):
         if not self._already_added_summaries.__contains__(tf.get_variable_scope().name+"/iterations_performed"):
             variable_summaries(number_of_iterations_performed, tf.get_variable_scope().name+"/iterations_performed")
             self._already_added_summaries.append(tf.get_variable_scope().name+"/iterations_performed")
+            variable_summaries(self.calculate_feature_entropy(final_output), tf.get_variable_scope().name + "/post_execution_output_entropy")
+            self._already_added_summaries.append(tf.get_variable_scope().name + "/post_execution_output_entropy"
 
     def loop_condition(self):
         return lambda input, state, iteration_number, iterate_prob, iteration_activations: \
@@ -69,7 +84,6 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
     def loop_body(self):
         return lambda input, state, iteration_number, iterate_prob, iteration_activations: \
                 self.resolve_iteration_calculation(input, state, iteration_number, iterate_prob, iteration_activations)
-
 
     def resolve_iteration_calculation(self, input, state, number_of_iterations_performed, iterate_prob, current_iteration_activations):
         output, new_state = self._internal_nn(input, state)
@@ -83,16 +97,18 @@ class IterativeCell(tf.nn.rnn_cell.RNNCell):
         number_of_iterations_performed += tf.floor(current_iteration_activations + iterate_prob)
         iteration_activations = self.resolve_iteration_activations(input, state, output, new_state, iterate_prob, current_iteration_activations)
         #output = tf.cond(self.loop_condition()(output, new_state, number_of_iterations_performed, iterate_prob, iteration_activations), lambda: input, lambda: output)
-        new_iterate_prob = tf.pow(iterate_prob, 2)
+        new_iterate_prob = iterate_prob * self._iterate_prob_decay_constant
         return output, new_state, number_of_iterations_performed, new_iterate_prob, iteration_activations
 
     def resolve_iteration_activations(self, input, old_state, output, new_state, iterate_prob, current_iteration_activations):
-        iteration_gate_logits = linear([input, new_state], self.output_size, True, scope=tf.get_variable_scope())
+        if self._iteration_activation_nn is not None:
+            iteration_activations = self._iteration_activation_nn.call(tf.concat(1,[input, new_state]))
+        else:
+            iteration_activations = sigmoid(linear([input, new_state], self.output_size, True, scope=tf.get_variable_scope()))
         tf.get_variable_scope().reuse_variables()
 
-        #iteration_activations = sigmoid(iteration_gate_logits)
-        iteration_activations = tf.floor(sigmoid(iteration_gate_logits) + iterate_prob)
-        return self.update_iteration_activations(current_iteration_activations, iteration_activations, iterate_prob)
+        discrete_iteration_activations = tf.floor(iteration_activations + iterate_prob)
+        return self.update_iteration_activations(current_iteration_activations, discrete_iteration_activations, iterate_prob)
 
     def update_iteration_activations(self, current_iteration_activations, new_iteration_activations, iterate_prob):
         # It is possible that other instances of the batch activate this cell, hence we need to avoid this by activate only those activations were this instance of the batch is actually activated
