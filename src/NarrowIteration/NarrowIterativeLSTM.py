6c843e43d3ee9f39bde88de222d3be646e803699
leandro <lpalma"fi.uba.ar>
2017-02-27 13:00:27 -0300

    "OLD_noresidual_activationfrominputnewh__updateTOTALLYinputstatefromnewh__decaying0.5probactivation0.5_nobatchreactivation_outputgateatlast_small"

--- src/NarrowIteration/NarrowIterativeLSTM.py
+++ src/NarrowIteration/NarrowIterativeLSTM.py
@@ -155,7 +155,7 @@ def update_iteration_activations(current_iteration_activations, new_iteration_ac
     # It is possible that other instances of the batch activate this cell, hence we need to avoid this
     # by activate only those activations were this instance of the batch is actually activated
     batch_iteration_activations = tf.reduce_max(current_iteration_activations, 1, True)
-    batch_iteration_activations_extended = tf.tile(batch_iteration_activations,[1, self.output_size])
+    batch_iteration_activations_extended = tf.tile(batch_iteration_activations,[1, current_iteration_activations.get_shape().dims[1].value])
 
     return new_iteration_activations * batch_iteration_activations_extended
 
