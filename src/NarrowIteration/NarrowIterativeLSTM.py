020a50f539f9d4f4a63064ba74136a256234f0a9
leandro <lpalma"fi.uba.ar>
2017-02-28 00:21:31 -0300

    "OLD_noresidual_activationfrominputnewh__updateTOTALLYstatefromnewh__noupdateinput_decaying0.5probactivation0.5_nobatchreactivation_outputgateatlast_small"

--- src/NarrowIteration/NarrowIterativeLSTM.py
+++ src/NarrowIteration/NarrowIterativeLSTM.py
@@ -114,7 +114,7 @@ def iterativeLSTM_Iteration(inputs, state, num_units, forget_bias, iteration_num
 
     new_c, new_h = array_ops.split(1, 2, new_state)
 
-    new_output = tf.cond(do_keep_looping, lambda: new_h, lambda: output)
+    new_output = tf.cond(do_keep_looping, lambda: inputs, lambda: output)
 
     return new_output, new_state, num_units, forget_bias, new_iteration_number, max_iterations, new_iteration_prob, iteration_prob_decay, new_iteration_activation, do_keep_looping
 
@@ -135,7 +135,7 @@ def iterativeLSTM(inputs, state, num_units, forget_bias, iteration_activation, i
     i, j, f, o = array_ops.split(1, 4, concat)
 
     new_c = c * sigmoid(f + forget_bias) + sigmoid(i) * tanh(j)
-    new_h = tanh(tanh(new_c)+inputs) 
+    new_h = tanh(new_c) 
 
     # Only a new state is exposed if the iteration gate in this unit of this batch activated the extra iteration.
     new_h = new_h * iteration_activation + h * (1 - iteration_activation)
