799e03e165902596a3279f168199b976350cda4a
leandro <lpalma"fi.uba.ar>
2017-04-05 00:33:24 -0300

    "OLD_activationfrominputoutput_updatecfromtanhREScTOTALLYinput_decaying0.5probactivation0.5_nobatchreactivation_small"

--- src/NarrowIteration/NarrowIterativeLSTM.py
+++ src/NarrowIteration/NarrowIterativeLSTM.py
@@ -136,7 +136,7 @@ def iterativeLSTM(inputs, state, num_units, forget_bias, iteration_activation, i
 
     new_info = tanh(j)
 
-    new_c = c * sigmoid(f + forget_bias) + sigmoid(i) * new_info  + inputs
+    new_c = c * sigmoid(f + forget_bias) + sigmoid(i) * tanh(new_info + inputs)
     new_h = tanh(new_c) * sigmoid(o)
 
     # Only a new state is exposed if the iteration gate in this unit of this batch activated the extra iteration.
@@ -145,10 +145,10 @@ def iterativeLSTM(inputs, state, num_units, forget_bias, iteration_activation, i
 
     new_state = array_ops.concat(1, [new_c, new_h])
 
-    new_output = tanh(new_h) * iteration_activation + inputs * (1 - iteration_activation)
+    new_output = new_h# * iteration_activation + inputs * (1 - iteration_activation)
 
     # In this approach the evidence of the iteration gate is based on the inputs that doesn't change over iterations and its state
-    p = linear([ inputs, new_output, new_h], num_units, True,scope= "iteration_activation")
+    p = linear([ inputs, new_output], num_units, True,scope= "iteration_activation")
 
 
     new_iteration_activation = update_iteration_activations(iteration_activation, floor(sigmoid(p) + iteration_prob))
