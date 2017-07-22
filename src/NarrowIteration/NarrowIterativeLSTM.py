c8f2127d0b41eb5ced93add5e8f30a9b2cd6ea5c
leandro <lpalma"fi.uba.ar>
2017-07-22 11:28:05 -0300

    "OLD_actfrominnewhout_outfromnewhRESinitcond_updatestatecfromtanhc,RESjbyiitcond_updateinfromout_nobatchreact_onelay200un_md"

--- src/NarrowIteration/NarrowIterativeLSTM.py
+++ src/NarrowIteration/NarrowIterativeLSTM.py
@@ -114,7 +114,7 @@ def iterativeLSTM_Iteration(inputs, state, num_units, forget_bias, iteration_num
 
     #new_c, new_h = array_ops.split(1, 2, new_state)
 
-    new_output = tf.cond(do_keep_looping, lambda: inputs, lambda: output)
+    #new_output = tf.cond(do_keep_looping, lambda: inputs, lambda: output)
 
     return output, new_state, num_units, forget_bias, new_iteration_number, max_iterations, new_iteration_prob, iteration_prob_decay, new_iteration_activation, do_keep_looping
 
