19b58a10f5287822a1a08c7ec7a1d46f12db39fc
leandro <lpalma"fi.uba.ar>
2017-08-05 20:51:28 -0300

    "OLD_actfrominout_outfromtanhnewh_RESinbyogateitcond_updatestatefromnewhitcond_cfromtanhc,RESjbyiitcond_updateinfromout_nobatchreact_onelay200un_md"

--- src/NarrowIteration/NarrowIterativeLSTM.py
+++ src/NarrowIteration/NarrowIterativeLSTM.py
@@ -143,7 +143,7 @@ def iterativeLSTM(inputs, state, num_units, forget_bias, iteration_activation, i
     concat = linear([iteration_count, inputs, h], 4 * num_units, True)
 
     # i = input_gate, j = new_input, f = forget_gate, o = output_gate
-    i, f, o, j = array_ops.split(1, 4, new_info)
+    i, f, o, j = array_ops.split(1, 4, concat)
 
     new_c = tanh(c * sigmoid(f + forget_bias)) + sigmoid(i) * tanh(j)
     new_h = tanh(new_c)
