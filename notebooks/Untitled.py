016e839291fa37c0a63b3bde867c1761e3f70f85
leandro <lpalma"fi.uba.ar>
2017-09-01 09:23:31 -0300

    "OLD_outfromnewhRESinitcond_updatestatefromnewhitcond_noupdatec_noupdatein_nobatchreact_fix6it_onelay650un_md"

---
+++ notebooks/Untitled.py
@@ -0,0 +1,88 @@
+
+# coding: utf-8
+
+# ## Recopilacion parcial de resultados
+
+# In[4]:
+
+import pandas as pd
+
+
+# In[2]:
+
+results_df = pd.DataFrame([{'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 1,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 2,
+                            'Perplexity': 89.51},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 3,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 4,
+                            'Perplexity': 88.90},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 5,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 6,
+                            'Perplexity': 88.14},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 7,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 8,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 9,
+                            'Perplexity': 95.56},
+                          {'Size': 'small', 
+                            'Training': '40 epochs', 
+                            'Learning rate scheme': 'decay by 0.8 after 14 epochs',
+                            'Units': 200,
+                            'Layers': 1,
+                            'Iterations': 10,
+                            'Perplexity': 95.56}])
+
+
+# In[3]:
+
+results_df
+
